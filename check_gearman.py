47cebfc5fab27f1e69cbcb3a7684c83f1e50e6a9
Zbigniew Olech <zbigniew.olech@farmaprom.pl>
2016-09-02 12:35:17 +0200

    Podawanie adresu i portu

--- check_gearman.py
+++ check_gearman.py
@@ -3,14 +3,19 @@ import gearman
 import json
 import sys
 
-gm_admin_client = gearman.GearmanAdminClient(['172.25.10.81:4730'])
 
-
-def discover_tasks():
+def discover_tasks(server, port):
     d = {'data': []}
+    gm_admin_client = gearman.GearmanAdminClient(
+        [':'.join([server, str(port)])])
     for task in gm_admin_client.get_status():
         d['data'].append({'{#TASK}': task['task']})
-    return json.dumps(d, indent=1)
+    return json.dumps(d, indent=1) # delete indent
 
 if __name__ == '__main__':
-    print discover_tasks()
+    server = sys.argv[1]
+    port = sys.argv[2]
+    api = sys.argv[3]
+
+    if api == 'discover':
+        print discover_tasks(server, port)
